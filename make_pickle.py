967865b69ffb0c5bcbc2dbf0c15823ba4b071d65
horace <hp264@cornell.edu>
2017-10-15 02:12:39 -0500

    add pickle script

---
+++ make_pickle.py
@@ -0,0 +1,9 @@
+import sys
+import argparse
+from utils.load_data import make_dataset_pickle
+
+gname = 'data/qm9.graph'
+lname = 'data/qm9.atoms'
+tname = 'data/qm9.target'
+pickle_name = 'data/qm9.pickle'
+make_dataset_pickle(gname, lname, tname, pickle_name)
