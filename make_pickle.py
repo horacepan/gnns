24977ace2e32d223a61c6f6c79714ede1f4c1730
Horace Pan <hopan@uchicago.edu>
2017-10-25 16:05:20 -0500

    add graph kernel datasets

--- make_pickle.py
+++ make_pickle.py
@@ -3,13 +3,15 @@ import argparse
 from utils.load_data import make_dataset_pickle
 import time
 
-gname = 'data/qm9.graph'
-lname = 'data/qm9.atoms'
-tname = 'data/qm9.target'
-pickle_name = 'data/qm9.pickle'
-start_time = time.time()
-print("Starting to pickle...")
-make_dataset_pickle(gname, lname, tname, pickle_name)
-print("Done pickling!")
-print("Time elapsed: {:.2f}".format(time.time() - start_time))
+for dataset in ['NCI1', 'NCI109', 'PTC', 'PROTEINS', 'MUTAG']:
+    gname = 'data/{}.graph'.format(dataset)
+    lname = 'data/{}.node'.format(dataset)
+    tname = 'data/{}.label'.format(dataset)
+    pickle_name = 'data/{}.pickle'.format(dataset)
+
+    start_time = time.time()
+    print("Starting to pickle...")
+    make_dataset_pickle(gname, lname, tname, pickle_name)
+    print("Done pickling!")
+    print("Time elapsed: {:.2f}".format(time.time() - start_time))
 
